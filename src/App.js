30e655db096b6873815720c6ab906ab52bcc4b30
Brian Thomas <Brianthomas571@gmail.com>
2020-08-04 17:09:03 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// api key: 305cf5d9dcc3ea93b3a6411ba033664b
+// firebase site NEEDS DEPLOY AFTER: https://netflix-88dd8.web.app/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">netflix clone</div>;
 }
 
 export default App;
