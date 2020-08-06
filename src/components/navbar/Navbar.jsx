73477988203c52a49cf0cb93f144fdd1b89e792f
Brian Thomas <Brianthomas571@gmail.com>
2020-08-05 22:24:56 -0500

    finishing navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import logo from "../../img/netflix-logo.png";
+import avatar from "../../img/avatar.jpg";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <nav></nav>;
+  const [show, setHandleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setHandleShow(true);
+      } else setHandleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <nav className={`nav-container ${show && "nav-black"}`}>
+      <img className="nav-logo" src={logo} alt="Netflix Logo" />
+      <img src={avatar} alt="Netflix Avatar" className="nav-avatar" />
+    </nav>
+  );
 };
 
 export default Navbar;
